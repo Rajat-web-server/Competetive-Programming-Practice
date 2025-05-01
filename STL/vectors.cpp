d5c1cd2e2431ea0d247956099615810bf9ee8267
Your Name <rajatpandey542@gmail.com>
2025-05-01 16:18:14 +0530

    Edit and Reverse property example added

--- STL/vectors.cpp
+++ STL/vectors.cpp
@@ -1,17 +1,38 @@
-#include<iostream>
-#include<vector>
-using namespace std ;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-int main () 
+int main()
 {
-   // vector
-   int x;
-   vector<int> v = {10,20,30,40};
-   v.push_back(50);
-   cout<<"vector: ";
-   for (auto x: v)
-    cout<<x <<" ";
-   cout<<"\n";
+    // vector
+    int x;
+    vector<int> v = {10, 20, 30, 40};
+    v.emplace_back(11);
+    v.push_back(50);
 
+    cout << "vector: ";
+    for (auto x : v)
+        cout << x << " ";
+    cout << "\n";
 
+    // Edit and reverse
+
+    v[1] = 25;
+    reverse(v.begin(), v.end());
+    cout << "reverse Vector :" << endl;
+    for (auto x : v)
+        cout << x << "\n";
+
+    v[1] = 25;
+    reverse(v.begin(), v.end());
+    cout << "reverse Vector :" << endl;
+    for (auto x : v)
+        cout << x << "\n";
+
+        v[3]=46;
+        /* for (auto x : v) */
+        cout<<"changed vector: "<<v[3];
+
+        //
 }
