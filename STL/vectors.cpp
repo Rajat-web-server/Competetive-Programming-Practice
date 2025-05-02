2d7381d9a9ed9a932e9328023bfdde9961368e70
Your Name <rajatpandey542@gmail.com>
2025-05-02 19:03:31 +0530

    copy functionality example added

--- STL/vectors.cpp
+++ STL/vectors.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void vector1()
 {
     // vector
-    int x;
+    
     vector<int> v = {10, 20, 30, 40};
     v.emplace_back(11);
     v.push_back(50);
@@ -50,12 +50,28 @@ void vector2()
     {
         cout << x << " ";
     }
+    cout<<endl;
      //Insert 
      vector<int> c = {11,22,33,44,55};
      v.insert(v.begin(),99);
      cout<<"After insert : ";
      for (int x : c) 
      cout<< x <<" ";
+     cout<<endl;
+
+     //Copy
+     vector <int> c2;
+     copy(c.begin(), c.end(), back_inserter(c2));
+     cout<<"copied vector : ";
+     for (int x: c2) cout <<x<<" ";
+     c2.emplace_back(33);
+     cout<<endl;
+     cout<<"emplace back result : ";
+     for (int x : c2) cout << x << " ";
+     c2.push_back(33);
+     cout<<endl;
+     cout<<"Push back result : ";
+     for (int x : c2) cout << x << " ";
      cout<<endl;
 
 }
