f10d75d3b4678633a2048448715ce4370286ce93
Your Name <rajatpandey542@gmail.com>
2025-05-02 18:46:29 +0530

    Insert functionality example added

--- STL/vectors.cpp
+++ STL/vectors.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void vector1()
 {
-  // vector
+    // vector
     int x;
     vector<int> v = {10, 20, 30, 40};
     v.emplace_back(11);
@@ -30,28 +30,37 @@ void vector1()
     for (auto x : v)
         cout << x << "\n";
 
-        v[3]=46;
-        /* for (auto x : v) */
-        cout<<"changed vector: "<<v[3]<<endl;
+    v[3] = 46;
+    /* for (auto x : v) */
+    cout << "changed vector: " << v[3] << endl;
 
-        //Auto
+    // Auto
 
-      for (auto it = v.end(); it !=v.begin(); it--) 
-      {cout<<"First element (auto) : "<<*it<<"\n";}  
+    for (auto it = v.end(); it != v.begin(); it--)
+    {
+        cout << "First element (auto) : " << *it << "\n";
+    }
 }
 
-void vector2(){
-    vector <int> v ={1,2,3,4,5};
+void vector2()
+{   // Erase
+    vector<int> v = {1, 2, 3, 4, 5};
     v.erase(v.end());
-    for (int x : v) 
+    for (int x : v)
     {
-        cout <<x<<" ";
+        cout << x << " ";
     }
+     //Insert 
+     vector<int> c = {11,22,33,44,55};
+     v.insert(v.begin(),99);
+     cout<<"After insert : ";
+     for (int x : c) 
+     cout<< x <<" ";
+     cout<<endl;
+
 }
 
 int main()
 {
-     vector2();
-       
-        
+    vector2();
 }
