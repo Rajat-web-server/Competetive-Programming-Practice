83bb0f85052d5db2498aed0f37f7bcff771b3b46
Your Name <rajatpandey542@gmail.com>
2025-05-13 19:24:38 +0530

    Pattern 3 is added

--- Pattern.cpp
+++ Pattern.cpp
@@ -13,7 +13,7 @@ void Pattern_1 ()
   }
 }
 
-void Pattern_2()
+void Pattern_2 ()
 {
   for (int i=0;i<4;i++)
   {
@@ -23,9 +23,20 @@ void Pattern_2()
     }
     cout<<endl;
   }
+}
 
+void Pattern_3 ()
+{
+  for (int i=1;i<5;i++)
+  {
+    for (int j=1;j<=i;j++)
+    {
+      cout<<j<<" ";
+    }
+    cout<<endl;
+  }
 }
 int main ()
 {
-    Pattern_2();
+    Pattern_3();
 }
