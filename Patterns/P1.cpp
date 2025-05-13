401c47161f05e50142421bf711ddad66f5000b08
Your Name <rajatpandey542@gmail.com>
2025-05-13 18:57:08 +0530

    Pattern 1 Added

---
+++ Patterns/P1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void Pattern_1 ()
+{
+  for (int i=0;i<4;i++)
+  {
+    for (int j=0;j<4;j++)
+    {
+        cout<<"*"<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+int main ()
+{
+    Pattern_1();
+}
