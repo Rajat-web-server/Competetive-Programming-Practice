15ce51f2229d2e4c59420df299e83d6358008c29
Your Name <rajatpandey542@gmail.com>
2025-05-29 20:18:56 +0530

    Recursion file created and one Program is added

---
+++ Recursion/name.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void name(int i,int n)
+{
+    if(i>n)
+    {
+      return ;  
+    }
+        
+   cout<<"Henry"<<endl;
+   name(i+1,n);
+}
+
+int main ()
+{
+    int n;
+    cin>>n;
+    name(1,n);
+    return 0;
+}
