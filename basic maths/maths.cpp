074e6579c8c74f42ca45251087aa3eb0ff80e9ba
Your Name <rajatpandey542@gmail.com>
2025-07-22 13:36:14 +0530

    counting digit functionality added

--- basic maths/maths.cpp
+++ basic maths/maths.cpp
@@ -1,20 +1,28 @@
 #include<iostream>
 using namespace std;
 
-void extraction_of_digit(){
-    int n;
+void extraction_of_digit(int n){
+    /* int n;
     cout<<"Enter the number :  ";
-    cin>>n;
+    cin>>n; */
+    int count=0;
     while(n>0)
     {   
         int lastDigit=n%10;
+        
         n=n/10;
+        count++;
         cout<<n<<endl;
+        
     }
+    cout<<"Digit count : "<<count;
 
 }
 
 int main ()
 {
-    extraction_of_digit();
+    int n;
+    cout<<"Enter the number :  ";
+    cin>>n;
+    extraction_of_digit(n);
 }
